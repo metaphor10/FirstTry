ff6e37d5569b50f0c897224a719a80aa5ab193c7
Michael Black <michaelblack@temp.home>
2013-02-02 00:12:19 -0500

    Initial Commit

---
+++ FirstTry/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstTry
+//
+//  Created by Michael Black on 2/2/13.
+//  Copyright (c) 2013 American University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AB1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AB1AppDelegate class]));
+    }
+}
